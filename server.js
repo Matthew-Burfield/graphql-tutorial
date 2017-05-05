d7b33c915bc54a9b98b7639814e1c4aa29751289
Matthew-Burfield <m1.burfield@gmail.com>
2017-05-05 14:07:58 +1000

    Added mutations for a message

--- server.js
+++ server.js
@@ -4,27 +4,64 @@ const { buildSchema } = require('graphql')
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
   type Query {
-    quoteOfTheDay: String
-    random: Float!
-    rollDice(numDice: Int!, numSides: Int): [Int]
+    getMessage(id: ID!): Message
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
   }
 `)
 
+// If Message had any complex fields, we'd put them on this object.
+class Message {
+  constructor(id, { content, author }) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
+
+// Maps username to content
+const fakeDatabase = {}
+
 // The root provides a resolver function for each API endpoint
 const root = {
-  quoteOfTheDay: () => {
-    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within'
-  },
-  random: () => {
-    return Math.random()
-  },
-  rollDice: ({numDice, numSides}) => {
-    const output = []
-    for (let i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)))
+  getMessage: ({id}) => {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ', id)
     }
-    return output
+    return new Message(id, fakeDatabase[id])
+  },
+
+  createMessage: ({input}) => {
+    // Create a random id for our "database"
+    const id = require('crypto').randomBytes(10).toString('hex')
+
+    fakeDatabase[id] = input
+    return new Message(id, input)
+  },
+
+  updateMessage: ({ id, input }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ', id)
+    }
+
+    // This replaces all old data, but some apps might want partial update.
+    fakeDatabase[id] = input
+    return new Message(id, input)
   }
 }
 
@@ -35,5 +72,5 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true,
 }))
 
-app.listen(4000)
-console.log('Running a GraphQL server at localhost:4000/graphql')
+app.listen(4000, () => console.log('Running a GraphQL server at localhost:4000/graphql'))
+
