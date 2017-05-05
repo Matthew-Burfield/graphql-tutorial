c65672a9a49be91a95c4c5877c21de79b6090445
Matthew-Burfield <m1.burfield@gmail.com>
2017-05-05 12:28:00 +1000

    added basic types

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
-var express = require('express')
-var graphqlHTTP = require('express-graphql')
-var { buildSchema } = require('graphql')
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
+const schema = buildSchema(`
   type Query {
-    hello: String
+    quoteOfTheDay: String
+    random: Float!
+    rollDice(numDice: Int!, numSides: Int): [Int]
   }
 `)
 
 // The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    return 'Hello world!'
+const root = {
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within'
   },
+  random: () => {
+    return Math.random()
+  },
+  rollDice: ({numDice, numSides}) => {
+    const output = []
+    for (let i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)))
+    }
+    return output
+  }
 }
 
-var app = express()
-
+const app = express()
 app.use('/graphql', graphqlHTTP({
   schema,
   rootValue: root,
